b325f4e6fb1708cdbb2775a346cbbd1e46e63f6a
miklik72 <miklik@miklik.cz>
2016-11-01 13:19:19 +0100

    Change temperature program

--- Zone3Termostat/Zone3Termostat.ino
+++ Zone3Termostat/Zone3Termostat.ino
@@ -178,15 +178,15 @@ byte prg_temp[PROGRAMS][DAY_STEP] =                           // temperature for
   {23,21,23,20,0,0},
   {24,20,0,0,0,0},
   {22,0,0,0,0,0},
-  {23,19,0,0,0,0},
-  {24,0,0,0,0,0}
+  {22,21,22,21,20,18},
+  {18,0,0,0,0,0}
 };
 unsigned int prg_time[PROGRAMS][DAY_STEP] =        // time points for programs
 {
   {600,1000,1500,2300,0,0},                              // number format HHMM, 0 = off
   {600,2200,0,0,0,0},
   {100,0,0,0,0,0},
-  {600,2300,0,0,0,0},
+  {600,800,1800,2000,2200,2300},
   {100,0,0,0,0,0}
 };
 unsigned long deltime = 0;
